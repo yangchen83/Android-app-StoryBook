53d1f99efbc4c416672701d27144634d360325cf
Yang Chen <yang1983chen@gmail.com>
2015-04-15 15:39:45 -0400

    finished opening page

---
+++ main/java/com/example/yangchen/storybook/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.yangchen.storybook;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    private EditText mNameField;
+    private Button mStartButton;
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                Toast.makeText(MainActivity.this, "Welcome aboard " + name + "!!!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
